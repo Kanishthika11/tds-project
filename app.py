6fdbd0ded5f176520b262181cbe9c0dc0883aa9b
Kanishthika11 <kkanishthika1@gmail.com>
2025-06-14 23:35:55 +0530

    Update2 app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from scraper import scrape_discourse
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # Root route (so homepage doesn’t return 404)
 @app.route('/')
@@ -34,5 +34,5 @@ def answer_question():
 
     return jsonify(response)
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
